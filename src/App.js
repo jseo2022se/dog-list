7738ac42ade42b589f630d2f72f098cadea24a5d
Jeonghyun Seo <jseo2022se@gmail.com>
2022-10-18 11:43:59 -0400

    new branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
     </div>
   );
 }
 
 export default App;
+
+
+/*
+
+1. API with pics
+
+2. React Router
+  - 5 Functional components
+  - 3 pages (besides app)
+    - must have index page
+    - show page
+    - page with a form for user input
+
+3. CSS framework
+
+4. Deployed to Netlify
+
+5. Commits
+
+*/
