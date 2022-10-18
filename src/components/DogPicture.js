6b6bdff744eaa5415a0cc7116f2f7272a4c97736
Dagny Mol <110190307+dagnymol81@users.noreply.github.com>
2022-10-18 14:32:39 -0400

    Update DogPicture.js

--- src/components/DogPicture.js
+++ src/components/DogPicture.js
@@ -1,23 +1,4 @@
-import { useState, useEffect } from 'react'
-
-export default function ShowDog({ breed }) {
-  
-  let [dog, setDog] = useState({})
-
-  const getDog = async () => {
-    try {
-      const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-      const data = await response.json()
-      setDog(data)
-    } catch(e) {
-      console.log(e)
-   }
-  }
-
-  useEffect(() => {
-    getDog()
-  }, [])
-  
+export default function ShowDog({ dog }) {
   return (
     <div>
       <img src={dog.message} alt="dog" />
