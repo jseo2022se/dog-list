fc00355e5eede84ffbeda3b952801add2211ce42
rvp15 <vedhagithub@gmail.com>
2022-10-18 13:54:48 -0400

    Progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
-import ShowDog from './components/DogPicture';
 
 function App() {
   return (
     <div className="App">
-      <ShowDog />
+     
     </div>
   );
 }
